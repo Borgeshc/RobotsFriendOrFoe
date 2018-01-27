f04a00ebadcf210570fbde1fda2b1ada0dd63d11
sgebhard <gebhas6@gmail.com>
2018-01-27 01:29:35 -0600

    Added animations and fixed ducking

--- Assets/Game/Scripts/AutoMove.cs
+++ Assets/Game/Scripts/AutoMove.cs
@@ -9,6 +9,7 @@ public class AutoMove : MonoBehaviour
     public float speed = 3;
     public float jumpForce = 300;
     public Transform ground;
+    public float crouchHeight = 1.75f;
 
     bool grounded = true;
     bool ducking;
@@ -16,10 +17,21 @@ public class AutoMove : MonoBehaviour
     BoxCollider col;
     Rigidbody rb;
 
+    Vector3 originalSize;
+    Vector3 originalCenter;
+    Animator anim;
+
+    float yPosition;
+
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<BoxCollider>();
+        anim = GetComponentInChildren<Animator>();
+
+        originalSize = col.size;
+        originalCenter = col.center;
+        yPosition = transform.position.y;
 	}
 	
 	void Update ()
@@ -39,6 +51,9 @@ public class AutoMove : MonoBehaviour
     public void Jump()
     {
         if (!grounded) return;
+
+        anim.SetTrigger("Jump");
+
         rb.AddForce(Vector3.up * jumpForce);
         grounded = false;
     }
@@ -46,9 +61,13 @@ public class AutoMove : MonoBehaviour
     public void Duck()
     {
         if (ducking) return;
+
+        anim.SetTrigger("Duck");
+
         ducking = true;
-        col.size = new Vector3(col.size.x, col.size.y * .5f, col.size.z);
-        col.center = new Vector3(col.center.x, col.size.y * .5f, col.center.z);
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        col.size -= new Vector3(col.size.x, crouchHeight, col.size.z);
+        col.center -= new Vector3(col.center.x, crouchHeight / 2, col.center.z);
         StartCoroutine(Ducking());
     }
 
@@ -56,8 +75,11 @@ public class AutoMove : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
 
-        col.size = Vector3.one;
-        col.center = Vector3.zero;
+        col.size += new Vector3(1, crouchHeight, 1);
+        col.center += new Vector3(0, crouchHeight / 2, 0);
+
+        rb.constraints = ~RigidbodyConstraints.FreezePosition;
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
         ducking = false;
     }
 }
