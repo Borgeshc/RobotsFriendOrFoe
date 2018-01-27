1d2153d5713ca688b30b10d4a9ca361b265a84cc
TechnicalFate <c.a.blanchard1997@gmail.com>
2018-01-26 22:52:33 -0600

    Upper&Lower Object Generation
    Generates upper and lower obstacles at random defined intervals.

--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    public float spawnTime = 2;
+    public float spawnTriggerTime = 2;
+    public float spawnTime = 0;
+    public Transform[] spawnPoints;
+    public GameObject wallObj;
 
-    // Use this for initialization
-    void Start ()
+    private void Update()
     {
-        InvokeRepeating("GenerateObj", spawnTime, spawnTime);
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        spawnTime += Time.deltaTime;
+
+        if(spawnTime >= spawnTriggerTime)
+        {
+            GenerateObj();
+            spawnTime = 0;
+        }
+        
+    }
+
+    void GenerateObj()
     {
-		
-	}
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(wallObj, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+        spawnTriggerTime = Random.Range(2, 6);
+    }
 }
