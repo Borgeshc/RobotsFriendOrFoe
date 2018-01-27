a66a6ad754425890d94ec56a003e1362607ccfef
TechnicalFate <c.a.blanchard1997@gmail.com>
2018-01-26 22:17:16 -0600

    Basic movement

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public float spawnTime = 2;
+
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("GenerateObj", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
