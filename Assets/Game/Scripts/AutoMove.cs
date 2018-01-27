ae0c35ba64176a881d576de8793cdc993cb347bd
akim <alton.kim1@gmail.com>
2018-01-27 00:26:01 -0600

    added ducking

--- Assets/Game/Scripts/AutoMove.cs
+++ Assets/Game/Scripts/AutoMove.cs
@@ -11,11 +11,15 @@ public class AutoMove : MonoBehaviour
     public Transform ground;
 
     bool grounded = true;
+    bool ducking;
+
+    BoxCollider col;
     Rigidbody rb;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
 	}
 	
 	void Update ()
@@ -41,6 +45,19 @@ public class AutoMove : MonoBehaviour
 
     public void Duck()
     {
-        print("Duck");
+        if (ducking) return;
+        ducking = true;
+        col.size = new Vector3(col.size.x, col.size.y * .5f, col.size.z);
+        col.center = new Vector3(col.center.x, col.size.y * .5f, col.center.z);
+        StartCoroutine(Ducking());
+    }
+
+    IEnumerator Ducking()
+    {
+        yield return new WaitForSeconds(1);
+
+        col.size = Vector3.one;
+        col.center = Vector3.zero;
+        ducking = false;
     }
 }
